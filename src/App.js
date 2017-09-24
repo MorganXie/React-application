8b86ad3cde45b0a07539625fb4b8d7ee158dc655
MorganXie <529303860@qq.com>
2017-09-24 14:22:01 +0800

    App 传一个函数给Todoinput

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import TodoInput from './TodoInput'
+import TodoItem from './TodoItem'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            newTodo: '',
+            todoList: [
+
+                {id: 1, title: '第一个待办'},
+                {id: 2, title: '第二个待办'},
+            ]
+        }
+    }
+
+    render() {
+
+        let todos = this.state.todoList.map((item, index) => {
+
+            return (
+                <li key={index}>
+                    <TodoItem todo={item}   />
+                </li>
+            )
+        });
+
+        return (
+            <div className="App">
+                <h1>我的待办</h1>
+                <div className="inputWrapper">
+                    <TodoInput content={this.state.newTodo} onSubmit={this.addTodo}/>
+                </div>
+                <ol>
+                    {todos}
+                </ol>
+            </div>
+        )
+    }
+
+    addTodo(){
+        console.log('我的添加一个todo了')
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 
 ReactDOM.render(
-    <h1>Hi，Xiexuan</h1>,
+    <App></App>,
     document.getElementById('root')
 );
 
