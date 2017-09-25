38413b35c54e67e635e465b0b2a7c1e14919aff0
MorganXie <529303860@qq.com>
2017-09-25 19:29:39 +0800

    complete the ToDoList

--- src/App.js
+++ src/App.js
@@ -8,41 +8,79 @@ class App extends Component {
         super(props);
         this.state = {
             newTodo: '',
-            todoList: [
-
-                {id: 1, title: '第一个待办'},
-                {id: 2, title: '第二个待办'},
-            ]
+            todoList: []
         }
     }
 
     render() {
 
-        let todos = this.state.todoList.map((item, index) => {
+        let todos = this.state.todoList.filter((item) => !item.deleted).map((item, index) => {
 
             return (
                 <li key={index}>
-                    <TodoItem todo={item}   />
+                    <TodoItem todo={item} onToggle={this.toggle.bind(this)}
+                              onDelete={this.delete.bind(this)}/>
+
                 </li>
             )
         });
+        console.log(todos);
 
         return (
             <div className="App">
                 <h1>我的待办</h1>
                 <div className="inputWrapper">
-                    <TodoInput content={this.state.newTodo} onSubmit={this.addTodo}/>
+                    <TodoInput content={this.state.newTodo}
+                               onChange={this.changeTitle.bind(this)}
+                               onSubmit={this.addTodo.bind(this)}
+                    />
+
                 </div>
-                <ol>
+                <ol className="todoList">
                     {todos}
                 </ol>
             </div>
         )
     }
 
-    addTodo(){
-        console.log('我的添加一个todo了')
+    changeTitle(event) {
+        this.setState({
+            newTodo: event.target.value,
+            todoList: this.state.todoList
+        })
+    }
+
+    toggle(e, todo) {
+        todo.status = todo.status === 'completed' ? '' : 'completed';
+        this.setState(this.state)
+    }
+
+    addTodo(event) {
+        this.state.todoList.push({
+            id: idMaker(),
+            title: event.target.value,
+            status: null,
+            deleted: false
+        });
+
+        this.setState({
+            newTodo: '',
+            todoList: this.state.todoList
+        })
+
+    }
+
+    delete(event, todo) {
+        todo.deleted = true;
+        this.setState(this.state)
     }
 }
 
 export default App;
+
+let id = 0;
+
+function idMaker() {
+    id += 1;
+    return id;
+}
--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,12 +1,18 @@
 import React,{Component} from 'react';
+import './TodoInput.css'
 export default class TodoInput extends Component{
     render(){
-        return <input type="text" defaultValue={this.props.content} onKeyPress={this.submit.bind(this)}  />
-
+        return <input type="text" value={this.props.content}
+                      className='TodoInput'
+                      onKeyPress={this.submit.bind(this)}
+                      onChange={this.changeTitle.bind(this)}/>
     }
     submit(e){
         if(e.key === 'Enter'){
-            this.props.onSubmit.call();
+            this.props.onSubmit(e);
         }
+    }
+    changeTitle(e){
+        this.props.onChange(e);
     }
 }
